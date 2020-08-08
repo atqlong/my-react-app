ba184ad5ad265fff6b7387d0615d7a61cb96bb9d
atqlong <40660903+atqlong@users.noreply.github.com>
2020-08-08 09:47:42 +0100

    lessions 1 & 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Header from "../components/header"
 
 const IndexPage = () => (
-  <Layout>
+  <div>
     <SEO title="Home" />
-    <h1>Learn to Design & Code React Apps!</h1>
-    <p>Complete course about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+    <Header />
+    <div className="Hero">
+      <div className="HeroGroup">
+        
+        <h1>Learn to Design & Code React Apps!</h1>
+        <p>Complete course about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+        
+      </div>
+    </div>
+  </div>
 )
 
 export default IndexPage
